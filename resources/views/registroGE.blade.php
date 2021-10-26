80cdd31e6f946f9c8e6f9850f843c17cabea1c75
adrianC56 <73123917+adrianC56@users.noreply.github.com>
2021-10-25 23:03:33 -0400

    Initial commit

---
+++ resources/views/registroGE.blade.php
@@ -0,0 +1,12 @@
+@extends('templates.plantillaprincipal')
+
+@section('titulo') 
+    Registro Grupo Empresa
+@endsection
+
+@section('contenido')
+    <div id='regGE'></div>
+@endsection
+    
+    
+   
